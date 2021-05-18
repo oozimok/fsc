20164629cb8531dc2b6eee0ff8e6bae45a9c29bc
Oleg Ozimok <oleg.tukavin@gmail.com>
2021-05-18 22:11:08 +0300

    extended algorithm

--- app.js
+++ app.js
@@ -2,12 +2,41 @@
 
 const BinaryFile = require('binary-file');
 
-const myBinaryFile1 = new BinaryFile('FSC_NU85056_0120007C_werk_703.fsc', 'r');
+const FSC_00A00001_werk = new BinaryFile('files/FSC_00A00001_werk.fsc', 'r');
+(async function () {
+    try {
+        await FSC_00A00001_werk.open();
+
+        await parse(FSC_00A00001_werk, log('FSC_00A00001_werk', 25));
+
+        await FSC_00A00001_werk.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+const FSC_00A00001_marketing = new BinaryFile('files/FSC_00A00001_marketing.fsc', 'r');
+(async function () {
+    try {
+        await FSC_00A00001_marketing.open();
+
+        await parse(FSC_00A00001_marketing, log('FSC_00A00001_marketing', 25));
+
+        await FSC_00A00001_marketing.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+
+const myBinaryFile1 = new BinaryFile('files/FSC_NU85056_0120007C_werk_703.fsc', 'r');
 (async function () {
     try {
         await myBinaryFile1.open();
 
-        await parse(myBinaryFile1, log('werk', 10));
+        await parse(myBinaryFile1, log('werk', 25));
 
         await myBinaryFile1.close();
 
@@ -16,12 +45,12 @@ const myBinaryFile1 = new BinaryFile('FSC_NU85056_0120007C_werk_703.fsc', 'r');
     }
 })();
 
-const myBinaryFile1_1 = new BinaryFile('FSC_NU85056_0120007C_werk_fscs_id_1027.fsc', 'r');
+const myBinaryFile1_1 = new BinaryFile('files/FSC_NU85056_0120007C_werk_fscs_id_1027.fsc', 'r');
 (async function () {
     try {
         await myBinaryFile1_1.open();
 
-        await parse(myBinaryFile1_1, log('werk 1', 10));
+        await parse(myBinaryFile1_1, log('werk 1', 25));
 
         await myBinaryFile1_1.close();
 
@@ -29,76 +58,76 @@ const myBinaryFile1_1 = new BinaryFile('FSC_NU85056_0120007C_werk_fscs_id_1027.f
         console.log(`There was an error: ${err}`);
     }
 })();
-//
-// const myBinaryFile2 = new BinaryFile('FSC_NU85056_0120007C_marketing_703.fsc', 'r');
-// (async function () {
-//     try {
-//         await myBinaryFile2.open();
-//
-//         await parse(myBinaryFile2, log('marketing', 10));
-//
-//         await myBinaryFile2.close();
-//
-//     } catch (err) {
-//         console.log(`There was an error: ${err}`);
-//     }
-// })();
-//
-// const myBinaryFile3 = new BinaryFile('FSC_NU85056_0120007C_kundle_703.fsc', 'r');
-// (async function () {
-//     try {
-//         await myBinaryFile3.open();
-//
-//         await parse(myBinaryFile3, log('kundle 0', 10));
-//
-//         await myBinaryFile3.close();
-//
-//     } catch (err) {
-//         console.log(`There was an error: ${err}`);
-//     }
-// })();
-//
-// const myBinaryFile3_1 = new BinaryFile('FSC_NU85056_0120007C_kundle_703_fgn_lang.fsc', 'r');
-// (async function () {
-//     try {
-//         await myBinaryFile3_1.open();
-//
-//         await parse(myBinaryFile3_1, log('kundle 1', 10));
-//
-//         await myBinaryFile3_1.close();
-//
-//     } catch (err) {
-//         console.log(`There was an error: ${err}`);
-//     }
-// })();
-//
-// const myBinaryFile3_2 = new BinaryFile('FSC_NU85056_0120007C_kundle_703_keine_indiv.fsc', 'r');
-// (async function () {
-//     try {
-//         await myBinaryFile3_2.open();
-//
-//         await parse(myBinaryFile3_2, log('kundle 2', 10));
-//
-//         await myBinaryFile3_2.close();
-//
-//     } catch (err) {
-//         console.log(`There was an error: ${err}`);
-//     }
-// })();
-//
-// const myBinaryFile4 = new BinaryFile('FSC_NU85056_0120007C_vertrieb_703.fsc', 'r');
-// (async function () {
-//     try {
-//         await myBinaryFile4.open();
-//
-//         await parse(myBinaryFile4, log('vertrieb', 10));
-//
-//         await myBinaryFile4.close();
-//
-//     } catch (err) {
-//         console.log(`There was an error: ${err}`);
-//     }
-// })();
+
+const myBinaryFile2 = new BinaryFile('files/FSC_NU85056_0120007C_marketing_703.fsc', 'r');
+(async function () {
+    try {
+        await myBinaryFile2.open();
+
+        await parse(myBinaryFile2, log('marketing', 25));
+
+        await myBinaryFile2.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+const myBinaryFile3 = new BinaryFile('files/FSC_NU85056_0120007C_kundle_703.fsc', 'r');
+(async function () {
+    try {
+        await myBinaryFile3.open();
+
+        await parse(myBinaryFile3, log('kundle 0', 25));
+
+        await myBinaryFile3.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+const myBinaryFile3_1 = new BinaryFile('files/FSC_NU85056_0120007C_kundle_703_fgn_lang.fsc', 'r');
+(async function () {
+    try {
+        await myBinaryFile3_1.open();
+
+        await parse(myBinaryFile3_1, log('kundle 1', 25));
+
+        await myBinaryFile3_1.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+const myBinaryFile3_2 = new BinaryFile('files/FSC_NU85056_0120007C_kundle_703_keine_indiv.fsc', 'r');
+(async function () {
+    try {
+        await myBinaryFile3_2.open();
+
+        await parse(myBinaryFile3_2, log('kundle 2', 25));
+
+        await myBinaryFile3_2.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
+
+const myBinaryFile4 = new BinaryFile('files/FSC_NU85056_0120007C_vertrieb_703.fsc', 'r');
+(async function () {
+    try {
+        await myBinaryFile4.open();
+
+        await parse(myBinaryFile4, log('vertrieb', 25));
+
+        await myBinaryFile4.close();
+
+    } catch (err) {
+        console.log(`There was an error: ${err}`);
+    }
+})();
 
 let parse = async (binary, log) => {
     let position = 0;
@@ -143,23 +172,35 @@ let parse = async (binary, log) => {
     log(position, 'bestellNr', bestellNr);
     position += 7;
 
+
     const requesterAlgorithm = await binary.readUInt8(position);
+    //log(position, 'requesterAlgorithm', requesterAlgorithm);
+    position += 1;
     switch(requesterAlgorithm) {
         case 0:
-            position += 2;
-            log(position, 'requesterId', await binary.readUInt16(position));
-            log(position, 'requesterTyp', 'Marketing');
-            position += 2;
+            const requesterAlgorithmTwo = await binary.readUInt8(position);
+            position += 1;
+            //log(position, 'requesterAlgorithmTwo', requesterAlgorithmTwo);
+            if (requesterAlgorithmTwo === 1) {
+                position -= 2;
+                log(position, 'requesterId', await binary.readUInt16(position));
+                log(position, 'requesterTyp', 'Werk');
+                position += 2;
+            } else {
+                log(position, 'requesterId', await binary.readUInt16(position));
+                log(position, 'requesterTyp', 'Marketing');
+                position += 2;
+            }
             break;
         case 2:
+            position -= 1;
             log(position, 'requesterId', await binary.readUInt16(position));
             log(position, 'requesterTyp', 'Werk');
             position += 2;
             break;
         case 32:
-            position += 1;
-            const requesterAlgorithmTwo = await binary.readUInt8(position);
-            if (requesterAlgorithmTwo === 32) {
+            const requesterAlgorithmThree = await binary.readUInt8(position);
+            if (requesterAlgorithmThree === 32) {
                 position += 1;
                 log(position, 'requesterId', await binary.readString(3, position));
                 log(position, 'requesterTyp', 'Vertrieb');
@@ -172,7 +213,7 @@ let parse = async (binary, log) => {
             break;
         default:
             log(position, 'requesterAlgorithm', `unknown = ${requesterAlgorithm}`);
-            process.exit();
+            return ;
     }
 
     const indivMerkmType = await binary.readUInt8(position);
@@ -194,7 +235,7 @@ let parse = async (binary, log) => {
             break;
         default:
             log(position, 'indivMerkmType', `unknown = ${indivMerkmType}`);
-            process.exit();
+            return ;
     }
 
     const fscsId = await binary.readUInt16(position);
